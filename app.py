3267aa06ebde0047f49a013d83751f2ab0d50170
Shouvik Bajpayee <shouvikbajpayee@Shouviks-MacBook-Air.local>
2023-12-26 21:51:34 +0530

    Implemented authentication

---
+++ app.py
@@ -0,0 +1,109 @@
+from flask import Flask, jsonify, request
+from uuid_extensions import uuid7str
+import os
+import json
+import copy
+from flask_cors import CORS
+from gp_hashing.generateHash import generateHash
+
+app = Flask(__name__, static_url_path="")
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.secret_key = "thisisasecretkey"
+CORS(app, origins="*")
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/", methods=["GET", "POST"])
+def default():
+    return "<h1 style='text-align:center'>404<h1>"
+
+@app.route("/api", methods=["GET", "POST"])
+def default_api():
+    return "<h1 style='text-align:center'>404<h1>"
+
+@app.route("/api/signup", methods=["POST"])
+def signup():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    password = generateHash(request.form.get("password"))
+    userid = uuid7str()
+    new_user = {
+        userid: {
+            "name": name,
+            "email": email,
+            "password": password
+        }
+    }
+    with open(f"{APP_ROOT}/db/users.json", "r") as json_file:
+        users = json.load(json_file)
+        users_backup = copy.deepcopy(users)
+    for _, user in users.items():
+        if user["email"] == email:
+            res = {
+                "status": "not-ok",
+                "message": "Email already in use!"
+            }
+            return jsonify(res)
+    try:
+        users.update(new_user)
+        with open(f"{APP_ROOT}/db/users.json", "w") as json_file:
+            json.dump(users, json_file, indent=2)
+        res = {
+            "status": "ok",
+            "message": "Account created!"
+        }
+        return jsonify(res)
+    except Exception:
+        with open(f"{APP_ROOT}/db/users.json", "w") as json_file:
+            json.dump(users_backup, json_file, indent=2)
+        res = {
+            "status": "not-ok",
+            "message": "Could not create account!"
+        }
+        return jsonify(res)
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    with open(f"{APP_ROOT}/db/users.json", "r") as json_file:
+        users = json.load(json_file)
+    for id, user in users.items():
+        if user["email"] == email and user["password"] == generateHash(password):
+            res = {
+                "status": "ok",
+                "userid": id
+            }
+            return jsonify(res)
+        elif user["email"] == email and not (user["password"] == generateHash(password)):
+            res = {
+                "status": "not-ok",
+                "message": "Email and password doesn't match!"
+            }
+            return jsonify(res)
+    else:
+        res = {
+            "status": "not-ok",
+            "message": "Cannot find your account!"
+        }
+        return jsonify(res)
+
+@app.route("/api/get/user/<userid>", methods=["POST"])
+def get_user_details(userid):
+    with open(f"{APP_ROOT}/db/users.json", "r") as json_file:
+        users = json.load(json_file)
+    if userid in users.keys():
+        res = {
+            "status": "ok",
+            "user": users[userid]
+        }
+        return jsonify(res)
+    else:
+        res = {
+            "status": "not-ok",
+            "message": "User not found!"
+        }
+        return jsonify(res)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9999, debug=True)
